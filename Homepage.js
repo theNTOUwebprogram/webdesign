f7e44df5c7800555d5b948c1747684fcacf0f0bd
hsuan91 <jeff20050901@gmail.com>
2024-11-25 12:21:53 +0800

    增加刪除按鈕

--- Homepage.js
+++ Homepage.js
@@ -2,6 +2,7 @@ let accountInput;
 let passwordInput;
 let have_account;
 let new_account;
+let delete_account;
 
 
 
@@ -10,9 +11,11 @@ function start() {
     passwordInput = document.getElementById("passwordInput");
     have_account= document.getElementById("have_account");
     new_account = document.getElementById("new_account");
+    delete_account = document.getElementById("delete_account");
     
     have_account.addEventListener("click", login, false);
     new_account.addEventListener("click", generateAccount, false);
+    delete_account.addEventListener("click",deleteAccount, false);
 }
 
 function login() {
@@ -84,6 +87,44 @@ function generateAccount() {
         passwordInput.value = "";
     }
     
+}
+
+function deleteAccount() {
+    let account = accountInput.value;
+    let password = passwordInput.value;
+    let flag = 0;
+    if ((account === "") && (password === "")) {
+        window.alert("請輸入帳號及密碼");
+    }
+    else if (account === "") {
+        window.alert("請輸入帳號");
+    }
+    else if (password === "") {
+        window.alert("請輸入密碼");
+    }
+    else {
+        for (let i = 0; i < localStorage.length; i++) {
+            if ((localStorage.key(i) === account ) && (CryptoJS.AES.decrypt(localStorage.getItem(account), localStorage.key(i)).toString(CryptoJS.enc.Utf8) ===  password)) {  
+                window.alert("帳號已刪除");
+                accountInput.value = "";
+                passwordInput.value = "";
+                flag = 1;
+                localStorage.removeItem(account);
+                break;
+            }
+            else if (localStorage.key(i) === account) {
+                window.alert("密碼錯誤，請重新輸入密碼");
+                passwordInput.value = "";
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0) {
+            window.alert("查無此帳號，請重新輸入帳號及密碼");
+            accountInput.value = "";
+            passwordInput.value = "";
+        }
+    }
 }
 
 /*function redirectToPage(password) {
