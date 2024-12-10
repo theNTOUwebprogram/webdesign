251b16de6da346e65f4395e2e9de4757bd110e6b
jamessu0530 <151006820+jamessu0530@users.noreply.github.com>
2024-12-10 16:27:52 +0800

    change

--- public/js/save_money.js
+++ public/js/save_money.js
@@ -580,7 +580,7 @@ function data_decrypt(encryptedData) {
 
 function getHoroscope() {
     const horoscope = document.getElementById("horoscope").value;
-    const url = `horoscope?name=${horoscope}`; // 指向代理伺服器
+    const url = `/horoscope?name=${horoscope}`; // 指向代理伺服器
 
     fetch(url)
         .then(response => response.json())
@@ -610,7 +610,7 @@ function getHoroscope() {
 }
 
 function convertToTraditional(simplifiedText, parent) {
-    const proxyUrl = "convert.js"; // 指向代理伺服器
+    const proxyUrl = "/convert.js"; // 指向代理伺服器
     const data = {
         text: simplifiedText,
         converter: "Taiwan"
